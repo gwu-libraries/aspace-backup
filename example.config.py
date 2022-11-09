54a4800d58799a821d950d01f913c2b7f8aad588
Dolsy Smith <dsmith@gwu.edu>
2022-11-09 15:19:02 -0500

    Example config updated

---
+++ example.config.py
@@ -0,0 +1,18 @@
+# ASPace API base URL
+BASE_URL = ''
+# Repository number (int)
+REPO = 2
+# API credentials
+USER = ''
+PASSWORD = ''
+# Adjust settings as needed
+# Use base_local_path to reflect local directory in which to store backup
+config = {'base_url': BASE_URL,
+         'repo': REPO,
+         'user': USER,
+         'password': PASSWORD,
+         'include_unpublished': 'false',
+         'include_daos': 'false',
+          'numbered_cs': 'false',
+          'base_local_path': ''
+         }
